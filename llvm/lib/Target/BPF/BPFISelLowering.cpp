bee2776268dee6b86022aeeb45c66597a7eb1645
Jack May <jack@solana.com>
2019-02-14 18:00:58 -0800

    Cleanup messaging

--- llvm/lib/Target/BPF/BPFISelLowering.cpp
+++ llvm/lib/Target/BPF/BPFISelLowering.cpp
@@ -264,7 +264,7 @@ SDValue BPFTargetLowering::LowerFormalArguments(
   }
 
   if (IsVarArg) {
-    fail(DL, DAG, "functions with VarArgs");
+    fail(DL, DAG, "functions with VarArgs are not supported");
   }
 
   return Chain;
@@ -415,7 +415,8 @@ BPFTargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
     return DAG.getNode(Opc, DL, MVT::Other, Chain);
   }
 
-  // Analize return values.
+  // Analyze return values.
+  assert(Outs.size() < 2 && "Too many return values");
   CCInfo.AnalyzeReturn(Outs, getHasAlu32() ? RetCC_BPF32 : RetCC_BPF64);
 
   SDValue Flag;
