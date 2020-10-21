81d58db6fb3cc80dea0fb6c61a2f2b233716696e
Sean Young <sean@mess.org>
2020-10-21 21:34:20 +0100

    lld bpf patches
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ lld/ELF/Arch/BPF.cpp
@@ -0,0 +1,76 @@
+//===- BPF.cpp ------------------------------------------------------------===//
+//
+//                             The LLVM Linker
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "InputFiles.h"
+#include "Symbols.h"
+#include "Target.h"
+#include "lld/Common/ErrorHandler.h"
+#include "llvm/Object/ELF.h"
+#include "llvm/Support/Endian.h"
+
+using namespace llvm;
+using namespace llvm::object;
+using namespace llvm::support::endian;
+using namespace llvm::ELF;
+using namespace lld;
+using namespace lld::elf;
+
+namespace {
+class BPF final : public TargetInfo {
+public:
+  BPF();
+  RelExpr getRelExpr(RelType Type, const Symbol &S,
+                     const uint8_t *Loc) const override;
+  void relocateOne(uint8_t *Loc, RelType Type, uint64_t Val) const override;
+};
+} // namespace
+
+BPF::BPF() { 
+  noneRel = R_BPF_NONE;
+  relativeRel = R_BPF_64_RELATIVE;
+}
+
+RelExpr BPF::getRelExpr(RelType Type, const Symbol &S,
+                        const uint8_t *Loc) const {
+  switch (Type) {
+    case R_BPF_64_32:
+      return R_PC;
+    case R_BPF_64_64:
+      return R_ABS;
+    default:
+      error(getErrorLocation(Loc) + "unrecognized reloc " + toString(Type));
+      return R_NONE;
+  }
+  return R_NONE;
+}
+
+void BPF::relocateOne(uint8_t *Loc, RelType Type, uint64_t Val) const {
+  switch (Type) {
+    case R_BPF_64_32: {
+      // Relocation of a symbol
+      write32le(Loc + 4, ((Val - 8) / 8) & 0xFFFFFFFF);
+      break;
+    }
+    case R_BPF_64_64: {  
+      // Relocation of a lddw instruction
+      // 64 bit address is divided into the imm of this and the following
+      // instructions, lower 32 first.
+      write32le(Loc + 4, Val & 0xFFFFFFFF);
+      write32le(Loc + 8 + 4, Val >> 32);
+      break;
+    }
+    default:
+      error(getErrorLocation(Loc) + "unrecognized reloc " + toString(Type));
+  }
+}
+
+TargetInfo *elf::getBPFTargetInfo() {
+  static BPF Target;
+  return &Target;
+}
