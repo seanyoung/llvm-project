b8a9d51893b93e42ff9b1ebc3732e4c9cca62e19
Sean Young <sean@mess.org>
2020-10-21 21:34:20 +0100

    lld bpf patches
    Signed-off-by: Sean Young <sean@mess.org>

--- lld/ELF/Target.h
+++ lld/ELF/Target.h
@@ -155,6 +155,7 @@ TargetInfo *getRISCVTargetInfo();
 TargetInfo *getSPARCV9TargetInfo();
 TargetInfo *getX86TargetInfo();
 TargetInfo *getX86_64TargetInfo();
+TargetInfo *getBPFTargetInfo();
 template <class ELFT> TargetInfo *getMipsTargetInfo();
 
 struct ErrorPlace {
